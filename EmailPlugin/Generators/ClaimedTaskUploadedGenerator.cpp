cb2a409b78046da7888c01b66d39f5fc0c10f12c
David O Carroll <spaceindaver0@gmail.com>
2013-05-17 11:24:14 +0100

    Updated Claimed Task Uploaded email
    Part of #21

--- EmailPlugin/Generators/ClaimedTaskUploadedGenerator.cpp
+++ EmailPlugin/Generators/ClaimedTaskUploadedGenerator.cpp
@@ -20,18 +20,36 @@ void ClaimedTaskUploadedGenerator::run()
     QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
     QSharedPointer<User> user = UserDao::getUser(db, emailMessage.user_id());
     QSharedPointer<Task> task = TaskDao::getTask(db, emailMessage.task_id());
+    QSharedPointer<Project> project = QSharedPointer<Project>();
+    QSharedPointer<Organisation> org = QSharedPointer<Organisation>();
 
     if (user.isNull() || task.isNull()) {
         error = "Unable to generate claimed task uploaded email. Unable to find relevant data ";
         error += "in the DB, searched for user id " + QString::number(emailMessage.user_id());
         error += " and task id " + QString::number(emailMessage.task_id());
+    } else {
+        project = ProjectDao::getProject(db, task->projectid());
+
+        if (project.isNull()) {
+            error += "Unable to generate claimed task uploaded email. Unable to find relevant data ";
+            error += "in the DB, searched for project id " + task->projectid();
+        } else {
+            org = OrganisationDao::getOrg(db, project->organisationid());
+
+            if (org.isNull()) {
+                error += "Unable to generate claimed task uploaded email. Unable to find relevant data ";
+                error += "in the DB, searched for organisation id " + project->organisationid();
+            }
+        }
     }
 
     if (error == "") {
         ctemplate::TemplateDictionary dict("claimed-task-uploaded");
 
         dict.SetValue("USERNAME", user->display_name());
+        dict.SetValue("SITE_NAME", settings.get("site.name").toStdString());
         dict.SetValue("TASK_TITLE", task->title());
+        dict.SetValue("ORG_NAME", org->name());
         QString uploadUrl = settings.get("site.url");
         uploadUrl += "task/" + QString::number(task->id()) + "/id";
         dict.SetValue("TASK_UPLOAD", uploadUrl.toStdString());
--- templates/emails/claimed-task-uploaded.tpl
+++ templates/emails/claimed-task-uploaded.tpl
@@ -4,8 +4,9 @@
 
 <p>
     You have successfully uploaded an output file for the task {{TASK_TITLE}}. If there
-    are any issues with the file you provided you may get feedback from the Organisation.
-    If you made any mistakes you can upload a new version <a href="{{TASK_UPLOAD}}">here</a>.
+    are any issues with the file you provided you may receive feedback from the organisation,
+    {{ORG_NAME}}. If you made any mistakes you can upload a new version 
+    <a href="{{TASK_UPLOAD}}">here</a>.
 </p>
 
 <p>
@@ -14,5 +15,5 @@
 
 <p>
     Sincerely,<br />
-    The SOLAS Match Team
+    The {{SITE_NAME}} Team
 </p>
