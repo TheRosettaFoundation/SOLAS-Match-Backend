dd0b34104060dfb8ce6ec3a27a0f2d4472dafbef
David O Carroll <spaceindaver0@gmail.com>
2013-08-28 16:20:27 +0100

    Possible fix for encoding issues
    Read email body as a utf8 encoded string instead of a std string

--- EmailPlugin/Generators/UserReferenceEmailGenerator.cpp
+++ EmailPlugin/Generators/UserReferenceEmailGenerator.cpp
@@ -145,7 +145,7 @@ void UserReferenceEmailGenerator::run()
         email->setSender(settings.get("site.system_email_address"));;
         email->addRecipient(QString::fromStdString(user->email()));
         email->setSubject(settings.get("site.name") + ": User Reference");
-        email->setBody(QString::fromStdString(email_body));
+        email->setBody(QString::fromUtf8(email_body.c_str()));
     } else {
         email = this->generateErrorEmail(error);
     }
