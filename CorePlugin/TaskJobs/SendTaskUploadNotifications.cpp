81e6fac71a28112df2f2d227cbfcda48fd6dd46b
Alan Barrett <alanabarrett0@gmail.com>
2018-09-11 18:19:21 +0100

    Make sure someone has claimed Paernt Task

--- CorePlugin/TaskJobs/SendTaskUploadNotifications.cpp
+++ CorePlugin/TaskJobs/SendTaskUploadNotifications.cpp
@@ -61,13 +61,14 @@ void SendTaskUploadNotifications::run()
                       QSharedPointer<Task> parentTask = TaskDao::getParentTranslationTask(db, task->id());
                       QSharedPointer<User> parentClaimer = TaskDao::getUserClaimedTask(db, parentTask->id());
                       TrackedTaskUploaded trackedUpload;
-                      trackedUpload.set_email_type(trackedUpload.email_type());
-                      trackedUpload.set_task_id(task->id());
-                      trackedUpload.set_translator_id(translator->id());
-                      trackedUpload.set_user_id(parentClaimer->id());
-                      body = trackedUpload.SerializeAsString();
-                      publisher.publish(exchange, topic, body);
-
+                      if (!parentClaimer.isNull()) {
+                          trackedUpload.set_email_type(trackedUpload.email_type());
+                          trackedUpload.set_task_id(task->id());
+                          trackedUpload.set_translator_id(translator->id());
+                          trackedUpload.set_user_id(parentClaimer->id());
+                          body = trackedUpload.SerializeAsString();
+                          publisher.publish(exchange, topic, body);
+                      }
                       if (task->tasktype() == TRANSLATION) {
                           QSharedPointer<Task> revisionTask = TaskDao::getMatchingRevisionTask(db, task->id());
                           if (!revisionTask.isNull()) {
