286d7a7d5fac9d6cb6e8e6e331cc9a913b0dbda1
Alan Barrett <alanabarrett0@gmail.com>
2018-11-22 14:59:46 +0000

    debug info

--- Common/protobufs/emails/JSON.cpp
+++ Common/protobufs/emails/JSON.cpp
@@ -6,6 +6,11 @@ namespace SolasMatch::Common::Protobufs::Emails {
 
 static bool JSON::isJSON(std::string json_serialized)
 {
+if (!QJsonDocument::fromJson(QByteArray(json_serialized.c_str(), json_serialized.length())).isNull()) {
+  qDebug() << "isJSON";
+} else {
+  qDebug() << "NOT isJSON";
+}
     // Not the most efficient way, but simpler and safer to implement
     return !QJsonDocument::fromJson(QByteArray(json_serialized.c_str(), json_serialized.length())).isNull();
 }
@@ -17,81 +22,97 @@ void JSON::ParseFromString(std::string json_serialized)
 
 int JSON::email_type()
 {
+qDebug() << "JSON::email_type(): " << QString::number(json_object.value("email_type").toInt(0));
     return json_object.value("email_type").toInt(0);
 }
 
 int JSON::user_id()
 {
+qDebug() << "JSON::user_id(): " << QString::number(json_object.value("user_id").toInt(0));
     return json_object.value("user_id").toInt(0);
 }
 
 int JSON::project_id()
 {
+qDebug() << "JSON::project_id(): " << QString::number(json_object.value("project_id").toInt(0));
     return json_object.value("project_id").toInt(0);
 }
 
 int JSON::task_id()
 {
+qDebug() << "JSON::task_id(): " << QString::number(json_object.value("task_id").toInt(0));
     return json_object.value("task_id").toInt(0);
 }
 
 int JSON::claimant_id()
 {
+qDebug() << "JSON::claimant_id(): " << QString::number(json_object.value("claimant_id").toInt(0));
     return json_object.value("claimant_id").toInt(0);
 }
 
 int JSON::translator_id()
 {
+qDebug() << "JSON::translator_id(): " << QString::number(json_object.value("translator_id").toInt(0));
     return json_object.value("translator_id").toInt(0);
 }
 
 int JSON::org_id()
 {
+qDebug() << "JSON::org_id(): " << QString::number(json_object.value("org_id").toInt(0));
     return json_object.value("org_id").toInt(0);
 }
 
 int JSON::org_member_id()
 {
+qDebug() << "JSON::org_member_id(): " << QString::number(json_object.value("org_member_id").toInt(0));
     return json_object.value("org_member_id").toInt(0);
 }
 
 int JSON::badge_id()
 {
+qDebug() << "JSON::badge_id(): " << QString::number(json_object.value("badge_id").toInt(0));
     return json_object.value("badge_id").toInt(0);
 }
 
 int JSON::site_admin_id()
 {
+qDebug() << "JSON::site_admin_id(): " << QString::number(json_object.value("site_admin_id").toInt(0));
     return json_object.value("site_admin_id").toInt(0);
 }
 
 int JSON::org_admin_id()
 {
+qDebug() << "JSON::org_admin_id(): " << QString::number(json_object.value("org_admin_id").toInt(0));
     return json_object.value("org_admin_id").toInt(0);
 }
 
 int JSON::file_version()
 {
+qDebug() << "JSON::file_version(): " << QString::number(json_object.value("file_version").toInt(0));
     return json_object.value("file_version").toInt(0);
 }
 
 std::string JSON::class_name()
 {
+qDebug() << "JSON::class_name(): " << QString::number(json_object.value("class_name").toString(""));
     return json_object.value("class_name").toString("");
 }
 
 std::string JSON::feedback()
 {
+qDebug() << "JSON::feedback(): " << QString::number(json_object.value("feedback").toString(""));
     return json_object.value("feedback").toString("");
 }
 
 std::string JSON::body()
 {
+qDebug() << "JSON::body(): " << QString::number(json_object.value("body").toString(""));
     return json_object.value("body").toString("");
 }
 
 std::string JSON::recipient_email()
 {
+qDebug() << "JSON::recipient_email(): " << QString::number(json_object.value("recipient_email").toString(""));
     return json_object.value("recipient_email").toString("");
 }
 
