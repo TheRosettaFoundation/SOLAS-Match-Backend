d3f79cb7fce5ccea82b99774fc68f74147d7704c
Alan Barrett <alanabarrett0@gmail.com>
2019-01-07 12:24:03 +0000

    Cutoff

--- EmailPlugin/Generators/UserTaskStreamEmailGenerator.cpp
+++ EmailPlugin/Generators/UserTaskStreamEmailGenerator.cpp
@@ -63,109 +63,118 @@ void UserTaskStreamEmailGenerator::run()
         dict.SetValue("SITE_NAME", std::string(settings.get("site.name").toLatin1().constData(), settings.get("site.name").toLatin1().length()));
 
         int project_id = 0;
+        int tasks_within_cutoff = 0;
         foreach (QSharedPointer<Task> task, userTasks) {
-            ctemplate::TemplateDictionary *taskSect = dict.AddSectionDictionary("TASK_SECT");
-            QString taskView = settings.get("site.url") + "task/" + QString::number(task->id()) + "/view";
-            taskSect->SetValue("TASK_VIEW", taskView.toStdString());
-            taskSect->SetValue("TASK_TITLE", Email::htmlspecialchars(task->title()));
+            QDateTime deadline_DT = QDateTime::fromString(QString::fromStdString(task->deadline()), "yyyy-MM-ddTHH:mm:ss");
+            if (deadline_DT > QDateTime::currentDateTimeUtc().addMonths(-settings.get("mail.task_stream_cutoff_months").toInt())) { // Only notify about tasks with deadline within last (3) months
+                ctemplate::TemplateDictionary *taskSect = dict.AddSectionDictionary("TASK_SECT");
+                QString taskView = settings.get("site.url") + "task/" + QString::number(task->id()) + "/view";
+                taskSect->SetValue("TASK_VIEW", taskView.toStdString());
+                taskSect->SetValue("TASK_TITLE", Email::htmlspecialchars(task->title()));
 
-            QString taskType;
-            switch (task->tasktype()) {
-            case CHUNKING:
-                taskType = "Chunking";
-                break;
-            case TRANSLATION:
-                taskType = "Translation";
-                break;
-            case PROOFREADING:
-                taskType = "Revising";
-                break;
-            case POSTEDITING:
-                taskType = "Post-Editing";
-                break;
-            default:
-                taskType = "Invalid Type";
-                break;
-            }
-            taskSect->SetValue("TASK_TYPE", taskType.toStdString());
-
-            Locale taskSourceLocale = task->sourcelocale();
-            taskSect->SetValue("SOURCE_LANGUAGE", taskSourceLocale.languagename());
-
-            Locale taskTargetLocale = task->targetlocale();
-            taskSect->SetValue("TARGET_LANGUAGE", taskTargetLocale.languagename());
-
-            taskSect->SetValue("WORD_COUNT", QString::number(task->wordcount()).toStdString());
-            QString createdTime = QDateTime::fromString(QString::fromStdString(task->createdtime()),
-                       "yyyy-MM-ddTHH:mm:ss").toString("d MMMM yyyy - hh:mm");
-            taskSect->SetValue("CREATED_TIME", createdTime.toStdString());
-            QString deadline = QDateTime::fromString(QString::fromStdString(task->deadline()),
-                    "yyyy-MM-ddTHH:mm:ss").toString("d MMMM yyyy - hh:mm");
-            taskSect->SetValue("DEADLINE_TIME", deadline.toStdString());
-
-            QSharedPointer<Project> project = ProjectDao::getProject(db, task->projectid());
-            if (!project.isNull()) {
-                if (task->projectid() != project_id) { // Display first time only
-                    taskSect->ShowSection("DESCRIPTION_SECT");
-                    taskSect->SetValue("PROJECT_DESCRIPTION", Email::uiCleanseHTMLNewlineAndTabs(project->description()));
-                    taskSect->SetValue("PROJECT_IMPACT", Email::uiCleanseHTMLNewlineAndTabs(project->impact()));
+                QString taskType;
+                switch (task->tasktype()) {
+                case CHUNKING:
+                    taskType = "Chunking";
+                    break;
+                case TRANSLATION:
+                    taskType = "Translation";
+                    break;
+                case PROOFREADING:
+                    taskType = "Revising";
+                    break;
+                case POSTEDITING:
+                    taskType = "Post-Editing";
+                    break;
+                default:
+                    taskType = "Invalid Type";
+                    break;
                 }
+                taskSect->SetValue("TASK_TYPE", taskType.toStdString());
 
-                if (task->projectid() != project_id) { // Display first time only
-                    QList<QSharedPointer<Tag> > taskTags = TagDao::getTaskTags(db, task->id());
-                    if (taskTags.count() > 0) {
-                        taskSect->ShowSection("TAGS_SECT");
-                        foreach (QSharedPointer<Tag> tag, taskTags) {
-                            ctemplate::TemplateDictionary *tagsList = taskSect->AddSectionDictionary("TAGS_LIST");
-                            QString tagDetails = settings.get("site.url") + "tag/" + QString::number(tag->id()) + "/";
-                            tagsList->SetValue("TAG_DETAILS", tagDetails.toStdString());
-                            tagsList->SetValue("TAG_LABEL", Email::htmlspecialchars(tag->label()));
+                Locale taskSourceLocale = task->sourcelocale();
+                taskSect->SetValue("SOURCE_LANGUAGE", taskSourceLocale.languagename());
+
+                Locale taskTargetLocale = task->targetlocale();
+                taskSect->SetValue("TARGET_LANGUAGE", taskTargetLocale.languagename());
+
+                taskSect->SetValue("WORD_COUNT", QString::number(task->wordcount()).toStdString());
+                QString createdTime = QDateTime::fromString(QString::fromStdString(task->createdtime()),
+                           "yyyy-MM-ddTHH:mm:ss").toString("d MMMM yyyy - hh:mm");
+                taskSect->SetValue("CREATED_TIME", createdTime.toStdString());
+                QString deadline = QDateTime::fromString(QString::fromStdString(task->deadline()),
+                        "yyyy-MM-ddTHH:mm:ss").toString("d MMMM yyyy - hh:mm");
+                taskSect->SetValue("DEADLINE_TIME", deadline.toStdString());
+
+                QSharedPointer<Project> project = ProjectDao::getProject(db, task->projectid());
+                if (!project.isNull()) {
+                    if (task->projectid() != project_id) { // Display first time only
+                        taskSect->ShowSection("DESCRIPTION_SECT");
+                        taskSect->SetValue("PROJECT_DESCRIPTION", Email::uiCleanseHTMLNewlineAndTabs(project->description()));
+                        taskSect->SetValue("PROJECT_IMPACT", Email::uiCleanseHTMLNewlineAndTabs(project->impact()));
+                    }
+
+                    if (task->projectid() != project_id) { // Display first time only
+                        QList<QSharedPointer<Tag> > taskTags = TagDao::getTaskTags(db, task->id());
+                        if (taskTags.count() > 0) {
+                            taskSect->ShowSection("TAGS_SECT");
+                            foreach (QSharedPointer<Tag> tag, taskTags) {
+                                ctemplate::TemplateDictionary *tagsList = taskSect->AddSectionDictionary("TAGS_LIST");
+                                QString tagDetails = settings.get("site.url") + "tag/" + QString::number(tag->id()) + "/";
+                                tagsList->SetValue("TAG_DETAILS", tagDetails.toStdString());
+                                tagsList->SetValue("TAG_LABEL", Email::htmlspecialchars(tag->label()));
+                            }
+                        }
+                    }
+
+                    if (task->projectid() != project_id) { // Display first time only
+                        taskSect->ShowSection("PARTOF_SECT");
+                        QString projectView = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/view";
+                        taskSect->SetValue("PROJECT_VIEW", projectView.toStdString());
+                        taskSect->SetValue("PROJECT_TITLE", Email::htmlspecialchars(project->title()));
+                        QSharedPointer<Organisation> org = OrganisationDao::getOrg(db, project->organisationid());
+                        if (!org.isNull()) {
+                            QString orgView = settings.get("site.url") + "org/" + QString::number(project->organisationid()) + "/profile";
+                            taskSect->SetValue("ORG_VIEW", orgView.toStdString());
+                            taskSect->SetValue("ORG_NAME", org->name());
+                        }
+
+                        if (project->imageuploaded() && project->imageapproved()) {
+                            taskSect->ShowSection("IMAGE_SECT");
+                            QString projectImage = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/image";
+                            taskSect->SetValue("PROJECT_IMAGE", projectImage.toStdString());
                         }
                     }
                 }
 
-                if (task->projectid() != project_id) { // Display first time only
-                    taskSect->ShowSection("PARTOF_SECT");
-                    QString projectView = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/view";
-                    taskSect->SetValue("PROJECT_VIEW", projectView.toStdString());
-                    taskSect->SetValue("PROJECT_TITLE", Email::htmlspecialchars(project->title()));
-                    QSharedPointer<Organisation> org = OrganisationDao::getOrg(db, project->organisationid());
-                    if (!org.isNull()) {
-                        QString orgView = settings.get("site.url") + "org/" + QString::number(project->organisationid()) + "/profile";
-                        taskSect->SetValue("ORG_VIEW", orgView.toStdString());
-                        taskSect->SetValue("ORG_NAME", org->name());
-                    }
+                project_id = task->projectid();
+                tasks_within_cutoff++;
+            }
+        }
 
-                    if (project->imageuploaded() && project->imageapproved()) {
-                        taskSect->ShowSection("IMAGE_SECT");
-                        QString projectImage = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/image";
-                        taskSect->SetValue("PROJECT_IMAGE", projectImage.toStdString());
-                    }
-                }
+        if (tasks_within_cutoff > 0) {
+            bool footer_enabled=(QString::compare("y", settings.get("email-footer.enabled")) == 0);
+            if (footer_enabled)
+            {
+                QString donate_link = settings.get("email-footer.donate_link");
+                ctemplate::TemplateDictionary* footer_dict = dict.AddIncludeDictionary("FOOTER");
+                QString footer_location = QString(TEMPLATE_DIRECTORY) + "emails/footer.tpl";
+                footer_dict -> SetValue("DONATE_LINK",donate_link.toStdString());
+                footer_dict -> SetFilename(footer_location.toStdString());
             }
 
-            project_id = task->projectid();
+            std::string email_body;
+            QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/user-task-stream.tpl";
+            ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
+
+            email->setSender(settings.get("site.system_email_address"));;
+            email->addRecipient(QString::fromStdString(user->email()));
+            email->setSubject(settings.get("site.name") + ": Task Stream");
+            email->setBody(QString::fromUtf8(email_body.c_str()));
+            this->emailQueue->insert(email, this->currentMessage);
+        } else {
+            qDebug() << "UserTaskStreamEmailGenerator: No tasks within cutoff for user " << QString::number(emailRequest.user_id());
         }
-
-        bool footer_enabled=(QString::compare("y", settings.get("email-footer.enabled")) == 0);
-        if (footer_enabled)
-        {
-            QString donate_link = settings.get("email-footer.donate_link");
-            ctemplate::TemplateDictionary* footer_dict = dict.AddIncludeDictionary("FOOTER");
-            QString footer_location = QString(TEMPLATE_DIRECTORY) + "emails/footer.tpl";
-            footer_dict -> SetValue("DONATE_LINK",donate_link.toStdString());
-            footer_dict -> SetFilename(footer_location.toStdString());
-        }
-
-        std::string email_body;
-        QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/user-task-stream.tpl";
-        ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-
-        email->setSender(settings.get("site.system_email_address"));;
-        email->addRecipient(QString::fromStdString(user->email()));
-        email->setSubject(settings.get("site.name") + ": Task Stream");
-        email->setBody(QString::fromUtf8(email_body.c_str()));
-        this->emailQueue->insert(email, this->currentMessage);
     } else {
         if (error != "") {
             email = this->generateErrorEmail(error);
