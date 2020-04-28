939f9f00d12bcca839c70c5de10bae2a409a8a19
Alan Barrett <alanabarrett0@gmail.com>
2020-04-28 17:05:08 +0100

    Another test

--- EmailPlugin/Smtp.cpp
+++ EmailPlugin/Smtp.cpp
@@ -66,6 +66,7 @@ void Smtp::send(QSharedPointer<Email> email)
     if (test) mail_message.addRecipient("eric.paquin@live.ie"); // TEST CODE
     if (test) mail_message.addRecipient("info@trommons.org"); // TEST CODE
     if (test) mail_message.addRecipient("alanabarrett0@yahoo.com"); // TEST CODE
+    if (test) mail_message.addRecipient("alanbarrett@therosettafoundation.org"); // TEST CODE
 
   if (!test || (QString::compare(only_send, email->getSubject()) == 0)) {
     recipientString.replace("@", "%40");
