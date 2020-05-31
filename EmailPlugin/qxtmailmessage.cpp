eb184a2b224b0001bbc360a25266de68a146396d
Alan Barrett <alanabarrett0@gmail.com>
2020-05-31 16:57:15 +0100

    Set "From" in settings

--- EmailPlugin/qxtmailmessage.cpp
+++ EmailPlugin/qxtmailmessage.cpp
@@ -214,7 +214,7 @@ QByteArray qxt_fold_mime_header(const QString& key, const QString& value, QTextC
 
     if (!prefix.isEmpty()) line += prefix;
 
-    if ((key != "Subject" && key != "From") && !value.contains("=?") && latin1->canEncode(value))
+    if ((key != "Subject") && !value.contains("=?") && latin1->canEncode(value))
     {
         bool firstWord = true;
         foreach(const QByteArray& word, value.toLatin1().split(' '))
@@ -327,7 +327,6 @@ QByteArray QxtMailMessage::rfc2822() const
 
     if (!sender().isEmpty() && !hasExtraHeader("From"))
     {
-qDebug() << "HERE111"; //REMOVE
         rv += qxt_fold_mime_header("From", sender(), latin1);
     }
 
@@ -405,7 +404,6 @@ qDebug() << "HERE111"; //REMOVE
             rv += qxt_fold_mime_header(listUnsubscribe.toLatin1(), extraHeader(r), latin1);
         }
         else {
-qDebug() << "HERE222" << r << ";" << extraHeader(r); //REMOVE
             rv += qxt_fold_mime_header(r.toLatin1(), extraHeader(r), latin1);
         }
     }
