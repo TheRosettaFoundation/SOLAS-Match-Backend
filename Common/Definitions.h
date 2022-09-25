314f4849d367fe7752db909e86182f930519072d
Alan Barrett <alanabarrett0@gmail.com>
2022-09-25 15:17:02 +0100

    WIP +struct task_type_item task_types[]

--- Common/Definitions.h
+++ Common/Definitions.h
@@ -5,11 +5,28 @@
 #include <QPair>
 
 enum TaskTypes {
-    CHUNKING = 1,
-    TRANSLATION = 2,
+    CHUNKING     = 1,
+    TRANSLATION  = 2,
     PROOFREADING = 3,
-    POSTEDITING = 4
+    POSTEDITING  = 4,
+    QUALITY      = 5,
+    APPROVAL     = 6
 };
+
+struct task_type_item {
+    int enum_type;
+    int enabled;
+    std::string type;
+};
+struct task_type_item task_types[] = {
+    {CHUNKING,     0, "Segmentation"},
+    {TRANSLATION,  1, "Translation"},
+    {PROOFREADING, 1, "Revising"},
+    {POSTEDITING,  0, "Desegmentation"},
+    {QUALITY,      0, "QA??"},
+    {APPROVAL,     1, "Approval??"}
+};
+const int task_types_count = 6;
 
 enum TaskStatus {
     WAITING_FOR_PREREQUISITES = 1,
