2d82734b6fe646b7a71c478151ac28c857a8f573
Alan Barrett <alanabarrett0@gmail.com>
2023-03-02 16:49:02 +0000

    If for revisor use: "task_completed_to_revising_volunteer"

--- EmailPlugin/Generators/TrackedTaskUploadedEmailGenerator.cpp
+++ EmailPlugin/Generators/TrackedTaskUploadedEmailGenerator.cpp
@@ -140,7 +140,11 @@ void TrackedTaskUploadedEmailGenerator::run()
         email->addRecipient(QString::fromStdString(user->email()));
         email->setSubject(settings.get("site.name") + ": Task Completed Notification");
         email->setBody(QString::fromUtf8(email_body.c_str()));
-        UserDao::log_email_sent(db, email_message.user_id(), email_message.task_id(), task->projectid(), project->organisationid(), email_message.translator_id(), 0, 0, "task_completed_to_subscribed_admin");
+        if (is_revisor_for_split_memsource_task) {
+            UserDao::log_email_sent(db, email_message.user_id(), email_message.task_id(), task->projectid(), project->organisationid(), email_message.translator_id(), 0, 0, "task_completed_to_revising_volunteer");
+        } else {
+            UserDao::log_email_sent(db, email_message.user_id(), email_message.task_id(), task->projectid(), project->organisationid(), email_message.translator_id(), 0, 0, "task_completed_to_subscribed_admin");
+        }
     } else {
         email = this->generateErrorEmail(error);
     }
