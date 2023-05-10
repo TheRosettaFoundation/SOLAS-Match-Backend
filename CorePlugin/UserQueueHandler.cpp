28c5f990c69d42d10e5776bf69ae255205c47025
Alan Barrett <alanabarrett0@gmail.com>
2023-05-10 17:53:19 +0100

    WIP

--- CorePlugin/UserQueueHandler.cpp
+++ CorePlugin/UserQueueHandler.cpp
@@ -18,7 +18,6 @@ UserQueueHandler::UserQueueHandler()
 void UserQueueHandler::run()
 {
     qDebug() << "Running UserQueueHandler on thread " << this->thread()->currentThreadId();
-    this->registerRequestTypes();
     ConfigParser settings;
 
     QTimer *message_queue_read_timer = new QTimer();
@@ -28,33 +27,33 @@ void UserQueueHandler::run()
 
 void UserQueueHandler::consumeFromQueue()
 {
-//OR SOEMTHING static QMutex mutex;
-//QMutexLocker locker(&mutex);
+    static QMutex mutex;
+    if (mutex.tryLock()) {
+        if (!QFileInfo::exists("/repo/SOLAS-Match-Backend/STOP_consumeFromQueue")) {
+            QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
+            QMap<QString, QVariant> queue_request = TaskDao::get_queue_request(db, 1);
+            if (!queue_request.isNull()) {
+                qDebug() << "UserQueueHandler: Message received";
+                switch (queue_request["type"]) {
+                    case 3?:
+                        TaskRevokedNotificationHandler = new TaskRevokedNotificationHandler();
+                        TaskRevokedNotificationHandler->run(queue_request["task_id"], queue_request["claimant_id"]);
+                    break;
+                    case 4?:
+                        TaskDao::update_statistics(db);
+                        // db->call("statsUpdateAll", "");
+                    break;
 
-    if (!QFileInfo::exists("/repo/SOLAS-Match-Backend/STOP_consumeFromQueue")) {
-        QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
-        QMap<QString, QVariant> queue_request = TaskDao::get_queue_request(db, 1);
-        if (!queue_request.isNull()) {
-            qDebug() << "UserQueueHandler: Message received";
-            switch (queue_request["type"]) {
-                case 3?:
-                    TaskRevokedNotificationHandler = new TaskRevokedNotificationHandler();
-                    TaskRevokedNotificationHandler->run(queue_request["task_id"], queue_request["claimant_id"]);
-                break;
-                case 4?:
-                    TaskDao::update_statistics(db);
-                    // db->call("statsUpdateAll", "");
-                break;
 
-
+                }
+                TaskDao::remove_queue_request(db, queue_request["id"]);
             }
-
-            TaskDao::remove_queue_request(db, queue_request["id"]);
         }
+        mutex.unlock();
     }
 }
-//    UserTaskStreamNotificationRequest streamReq = UserTaskStreamNotificationRequest();
-//    qRegisterMetaType<TaskStreamNotificationHandler>(QString::fromStdString(streamReq.class_name()).toLatin1());
-
 //    OrgCreatedNotificationRequest orgCreated = OrgCreatedNotificationRequest();
 //    qRegisterMetaType<OrgCreatedNotifications>(QString::fromStdString(orgCreated.class_name()).toLatin1());
+
+//    UserTaskStreamNotificationRequest streamReq = UserTaskStreamNotificationRequest();
+//    qRegisterMetaType<TaskStreamNotificationHandler>(QString::fromStdString(streamReq.class_name()).toLatin1());
