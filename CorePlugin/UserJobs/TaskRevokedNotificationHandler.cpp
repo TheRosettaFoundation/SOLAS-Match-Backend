5b1353aca4781b6fbbc604f57efb26e923f58be3
Alan Barrett <alanabarrett0@gmail.com>
2023-05-10 11:35:43 +0100

    Remove Rabbit MQ

--- CorePlugin/UserJobs/TaskRevokedNotificationHandler.cpp
+++ CorePlugin/UserJobs/TaskRevokedNotificationHandler.cpp
@@ -20,75 +20,34 @@ using namespace SolasMatch::Common::Protobufs::Models;
 
 TaskRevokedNotificationHandler::TaskRevokedNotificationHandler()
 {
-    this->message = NULL;
-}
-
-TaskRevokedNotificationHandler::TaskRevokedNotificationHandler(AMQPMessage *proto)
-{
-    this->message = proto;
 }
 
 TaskRevokedNotificationHandler::~TaskRevokedNotificationHandler()
 {
 }
 
-void TaskRevokedNotificationHandler::run()
+void TaskRevokedNotificationHandler::run(task_id, claimant_id)
 {
     qDebug() << "Starting new thread to handle task revoked notifications";
-    QString exchange = "SOLAS_MATCH";
-    QString topic = "email.task.revoked";
-    uint32_t length = 0;
-    char *body = this->message->getMessage(&length);
-
-    if (length > 0) {
 
     QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
-
-    MessagingClient client;
-    if (client.init()) {
-            JSON notification;
-            if (notification.ParseFromString(std::string(body, length))) {
-                QList<QSharedPointer<User> > subscribedUsers = TaskDao::getSubscribedUsers(db, notification.task_id());
-                if (subscribedUsers.length() > 0) {
+    QList<QSharedPointer<User> > subscribedUsers = TaskDao::getSubscribedUsers(db, task_id);
+    if (subscribedUsers.length() > 0) {
                     OrgTaskRevokedEmail subscriberNotification;
-                    subscriberNotification.set_claimant_id(notification.claimant_id());
-                    subscriberNotification.set_task_id(notification.task_id());
+                    subscriberNotification.set_claimant_id(claimant_id);
+                    subscriberNotification.set_task_id(task_id);
                     subscriberNotification.set_email_type(subscriberNotification.email_type());
                     foreach (QSharedPointer<User> user, subscribedUsers) {
                         subscriberNotification.set_user_id(user->id());
                         client.publish(exchange, topic, subscriberNotification.SerializeAsString());
                     }
-                } else {
-                    qDebug() << "TaskRevokedNotification: No users subscribed to task";
-                }
-
-                UserTaskRevokedEmail userNotification;
-                userNotification.set_task_id(notification.task_id());
-                userNotification.set_user_id(notification.claimant_id());
-                userNotification.set_email_type(userNotification.email_type());
-                client.publish(exchange, topic, userNotification.SerializeAsString());
-            } else {
-                qDebug() << "TaskRevokedNotificationHandler: Failed to parse message body";
-            }
-qDebug() << "TaskRevokedNotificationHandler length: " << QString::number(length);
-uint32_t charvalue;
-for (uint32_t indexstr=0; indexstr<length; indexstr++) {
-  charvalue = body[indexstr];
-  if (charvalue >= 128) {
-    qDebug() << "Value: (" << QString::number(indexstr) << ") " << QString::number(charvalue-4294967040);
-  } else {
-    qDebug() << "Value: (" << QString::number(indexstr) << ") " << QString::number(charvalue);
-  }
-}
     } else {
-        qDebug() << "TaskRevokedNotificationHandler: Failed to initialise RabbitMQ Client";
+        qDebug() << "TaskRevokedNotification: No users subscribed to task";
     }
-  } else {
-    qDebug() << "TaskRevokedNotificationHandler: Unable to parse message body, length is 0";
-  }
-}
 
-void TaskRevokedNotificationHandler::setAMQPMessage(AMQPMessage *message)
-{
-    this->message = message;
+    UserTaskRevokedEmail userNotification;
+    userNotification.set_task_id(task_id);
+    userNotification.set_user_id(claimant_id);
+    userNotification.set_email_type(userNotification.email_type());
+    client.publish(exchange, topic, userNotification.SerializeAsString());
 }
--- CorePlugin/UserQueueHandler.cpp
+++ CorePlugin/UserQueueHandler.cpp
@@ -31,88 +31,36 @@ void UserQueueHandler::run()
     qDebug() << "Running UserQueueHandler on thread " << this->thread()->currentThreadId();
     this->registerRequestTypes();
     ConfigParser settings;
-    QString exchange = settings.get("messaging.exchange");
-    QString topic = "users.#";
-    QString queue = "CoreUsersQueue";
-
-    client = new MessagingClient();
-    client->init();
-    connect(client, SIGNAL(AMQPMessageReceived(AMQPMessage*)), this, SLOT(messageReceived(AMQPMessage*)));
-    connect(client, SIGNAL(AMQPError(QString)), this, SLOT(handleAMQPError(QString)));
-    client->declareQueue(exchange, topic, queue);
 
     QTimer *message_queue_read_timer = new QTimer();
-    connect(message_queue_read_timer, SIGNAL(timeout()), client, SLOT(consumeFromQueue()));
+    connect(message_queue_read_timer, SIGNAL(timeout()), this, SLOT(consumeFromQueue()));
     message_queue_read_timer->start(settings.get("messaging.poll_rate").toInt());
 }
 
-void UserQueueHandler::messageReceived(AMQPMessage *message)
+void UserQueueHandler::consumeFromQueue()
 {
-    qDebug() << "UserQueueHandler: Message received";
+    if (!QFileInfo::exists("/repo/SOLAS-Match-Backend/STOP_consumeFromQueue")) {
+        QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
+        QMap<QString, QVariant> queue_request = TaskDao::get_queue_request(db, 1);
+        if (!queue_request.isNull()) {
+            qDebug() << "UserQueueHandler: Message received";
+            switch (queue_request["type"]) {
+                case 3:
+                    UserTaskRevokedGenerator = new UserTaskRevokedGenerator();
+                    UserTaskRevokedGenerator->run(queue_request["task_id"], queue_request["claimant_id"]);
+                break;
+            }
 
-    AMQPQueue *messageQueue = message->getQueue();
-    if(messageQueue != NULL)
-    {
-        messageQueue->Ack(message->getDeliveryTag());
+            TaskDao::remove_queue_request(db, queue_request["id"]);
+        }
     }
-
-    uint32_t length = 0;
-    char *body = message->getMessage(&length);
- if (length > 0) {
-
-  if (SolasMatch::Common::Protobufs::Emails::JSON::isJSON(std::string(body, length))) {
-    SolasMatch::Common::Protobufs::Emails::JSON requestMessage;
-    requestMessage.ParseFromString(std::string(body, length));
-
-    int classId = QMetaType::type(QString::fromStdString(requestMessage.class_name()).toLatin1());
-    if (classId == 0) {
-        qDebug() << "UserQueueHandler JSON: Invalid proto type: " << QString::fromStdString(requestMessage.class_name());
-    } else {
-        JobInterface *runnable = static_cast<JobInterface *>(QMetaType::create(classId));
-        runnable->setAMQPMessage(message);
-        this->mThreadPool->start(runnable);
-    }
-  } else {
-    RequestMessage requestMessage;
-    requestMessage.ParseFromString(std::string(body, length));
-
-    int classId = QMetaType::type(QString::fromStdString(requestMessage.class_name()).toLatin1());
-    if (classId == 0) {
-        qDebug() << "UserQueueHandler: Invalid proto type: " << QString::fromStdString(requestMessage.class_name());
-    } else {
-        JobInterface *runnable = static_cast<JobInterface *>(QMetaType::create(classId));
-        runnable->setAMQPMessage(message);
-        this->mThreadPool->start(runnable);
-    }
-  }
- } else {
-     qDebug() << "UserQueueHandler: Unable to parse message body, length is 0";
- }
 }
+//    StatisticsUpdateRequest statUpdate = StatisticsUpdateRequest();
+//    qRegisterMetaType<StatisticsUpdate>(QString::fromStdString(statUpdate.class_name()).toLatin1());
+//    UserTaskStreamNotificationRequest streamReq = UserTaskStreamNotificationRequest();
+//    qRegisterMetaType<TaskStreamNotificationHandler>(QString::fromStdString(streamReq.class_name()).toLatin1());
+//    OrgCreatedNotificationRequest orgCreated = OrgCreatedNotificationRequest();
+//    qRegisterMetaType<OrgCreatedNotifications>(QString::fromStdString(orgCreated.class_name()).toLatin1());
+//    TaskRevokedNotification taskRevoked = TaskRevokedNotification();
+//    qRegisterMetaType<TaskRevokedNotificationHandler>(QString::fromStdString(taskRevoked.class_name()).toLatin1());
 
-void UserQueueHandler::handleAMQPError(QString error)
-{
-    qDebug() << "UserQueueHandler: AMQPError: " << error;
-}
-
-void UserQueueHandler::setThreadPool(QThreadPool *tp)
-{
-    this->mThreadPool = tp;
-}
-
-bool UserQueueHandler::isEnabled()
-{
-    return true;
-}
-
-void UserQueueHandler::registerRequestTypes()
-{
-    StatisticsUpdateRequest statUpdate = StatisticsUpdateRequest();
-    qRegisterMetaType<StatisticsUpdate>(QString::fromStdString(statUpdate.class_name()).toLatin1());
-    UserTaskStreamNotificationRequest streamReq = UserTaskStreamNotificationRequest();
-    qRegisterMetaType<TaskStreamNotificationHandler>(QString::fromStdString(streamReq.class_name()).toLatin1());
-    OrgCreatedNotificationRequest orgCreated = OrgCreatedNotificationRequest();
-    qRegisterMetaType<OrgCreatedNotifications>(QString::fromStdString(orgCreated.class_name()).toLatin1());
-    TaskRevokedNotification taskRevoked = TaskRevokedNotification();
-    qRegisterMetaType<TaskRevokedNotificationHandler>(QString::fromStdString(taskRevoked.class_name()).toLatin1());
-}
