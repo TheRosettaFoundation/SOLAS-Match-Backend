454c5abcaf2d7da4128f71633a9deee9bd2a535e
Alan Barrett <alanabarrett0@gmail.com>
2023-05-15 20:05:12 +0100

    U

--- CorePlugin/ProjectQueueHandler.cpp
+++ CorePlugin/ProjectQueueHandler.cpp
@@ -107,7 +107,7 @@ void ProjectQueueHandler::consumeFromQueue()
                         TaskClaimedEmailGenerator::run(queue_request["user_id"].toInt(), queue_request["task_id"].toInt(), queue_request["claimant_id"].toInt());
                         break;
                     case TaskUploadNotificationRequest:
-                        XSendTaskuploadNotifications::run(queue_request["task_id"].toInt());
+                        SendTaskUploadNotifications::run(queue_request["task_id"].toInt());
                         break;
                     case TaskRevokedNotification:
                         TaskRevokedNotificationHandler::run(queue_request["claimant_id"].toInt(), queue_request["task_id"].toInt());
