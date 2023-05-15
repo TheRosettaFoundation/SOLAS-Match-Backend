9c030ad274bd80b740394ae3cc07ef83c5d7d2fb
Alan Barrett <alanabarrett0@gmail.com>
2023-05-15 18:50:04 +0100

    static

--- EmailPlugin/Generators/UserTaskStreamEmailGenerator.h
+++ EmailPlugin/Generators/UserTaskStreamEmailGenerator.h
@@ -6,7 +6,7 @@
 class UserTaskStreamEmailGenerator : public IEmailGenerator
 {
 public:
-    void run(int user_id);
+    static void run(int user_id);
 };
 
 #endif // USERTASKSTREAMEMAILGENERATOR_H
