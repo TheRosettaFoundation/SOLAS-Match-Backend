4e1cbc3882f4d74bf13b34d78fd2a48d18cbbdc9
Alan Barrett <alanabarrett0@gmail.com>
2023-05-15 15:21:30 +0100

    ;

--- Common/DataAccessObjects/UserDao.h
+++ Common/DataAccessObjects/UserDao.h
@@ -62,13 +62,13 @@ public:
     static QList<QSharedPointer<User> > getRecordWarningUsers(QSharedPointer<MySQLHandler> db);
     static void insertWillBeDeletedUser(QSharedPointer<MySQLHandler> db, int user_id);
     static void log_email_sent(QSharedPointer<MySQLHandler> db, int recipient_id, int task_id, int project_id, int org_id, int translator_id, int admin_id, int badge_id, QString topic);
-    static void queue_email(QSharedPointer<MySQLHandler> db, int recipient_id, QString recipient, QString subject, QString body, int priority = 3)
-    static QMap<QString, QVariant> get_email_request(QSharedPointer<MySQLHandler> db)
-    static void mark_email_request_sent(QSharedPointer<MySQLHandler> db, int id)
-    static void insert_queue_request(QSharedPointer<MySQLHandler> db, int queue, int type)
-    static QMap<QString, QVariant> get_queue_request(QSharedPointer<MySQLHandler> db, int queue)
-    static void mark_queue_request_handled(QSharedPointer<MySQLHandler> db, int id)
-    static void update_statistics(QSharedPointer<MySQLHandler> db)
+    static void queue_email(QSharedPointer<MySQLHandler> db, int recipient_id, QString recipient, QString subject, QString body, int priority = 3);
+    static QMap<QString, QVariant> get_email_request(QSharedPointer<MySQLHandler> db);
+    static void mark_email_request_sent(QSharedPointer<MySQLHandler> db, int id);
+    static void insert_queue_request(QSharedPointer<MySQLHandler> db, int queue, int type);
+    static QMap<QString, QVariant> get_queue_request(QSharedPointer<MySQLHandler> db, int queue);
+    static void mark_queue_request_handled(QSharedPointer<MySQLHandler> db, int id);
+    static void update_statistics(QSharedPointer<MySQLHandler> db);
     static QList<int> get_users_list_for_earthquake(QSharedPointer<MySQLHandler> db);
     static QList<QSharedPointer<Task> > get_user_earthquake_tasks(QSharedPointer<MySQLHandler> db, int userId);
 };
