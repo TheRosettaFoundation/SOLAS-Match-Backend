b2bf7de1814a1d6497d6766155afda0db7795c63
Alan Barrett <alanabarrett0@gmail.com>
2023-05-20 12:52:40 +0100

    60 seconds to match current TWB

--- restart_backend.sh
+++ restart_backend.sh
@@ -1,10 +1,10 @@
 # restart_backend.sh
-# Stop C++ gracefully (i.e. first stop consuming messages from rabbitmq) and restart
-# Ideally this should not be done between 7 and 7:20 UTC because main deadline check (cron 7:05 UTC) is just time based
+# Runs at 4:50 UTC
+# Stop C++ gracefully (i.e. first stop consuming records from PHP and timed tasks from database) and restart
 
 cd /repo/SOLAS-Match-Backend
 touch /repo/SOLAS-Match-Backend/STOP_consumeFromQueue
-sleep 10
+sleep 60
 kill `ps -ef | grep PluginHandler | grep -v grep | grep -Eo '[0-9]+' | head --lines 1`
 rm /repo/SOLAS-Match-Backend/STOP_consumeFromQueue
 ./run_daemon.sh &disown
