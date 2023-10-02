10962defaf2b27e66b8956dfc2b628d46b65e668
Alan Barrett <alanabarrett0@gmail.com>
2023-10-02 15:25:47 +0100

    invite

--- CorePlugin/ProjectQueueHandler.cpp
+++ CorePlugin/ProjectQueueHandler.cpp
@@ -100,7 +100,7 @@ void ProjectQueueHandler::consumeFromQueue()
                         UserTaskCancelledEmailGenerator::run(queue_request["user_id"].toInt(), queue_request["task_id"].toInt());
                         break;
                     case OrgInvite:
-                        UserTaskCancelledEmailGenerator::run(queue_request["badge_id"].toInt());
+                        invite::run(queue_request["badge_id"].toInt());
                         break;
                 }
                 UserDao::mark_queue_request_handled(db, queue_request["id"].toULongLong());
