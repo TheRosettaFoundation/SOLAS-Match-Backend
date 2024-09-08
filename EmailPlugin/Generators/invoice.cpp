6bfb246f2995bbf9662b701139a3760e1a57ca8c
Alan Barrett <alanabarrett0@gmail.com>
2024-09-08 11:40:36 +0100

    invoice

--- EmailPlugin/Generators/invoice.cpp
+++ EmailPlugin/Generators/invoice.cpp
@@ -9,73 +9,25 @@ void invoice::run(int user_id, int badge_id)
     ConfigParser settings;
     QString error = "";
     QSharedPointer<MySQLHandler> db = MySQLHandler::getInstance();
-    QMap<QString, QVariant> special_registration = UserDao::get_special_registration_record(db, special_registration_id, settings.get("site.reg_key"));
-    if (special_registration.isEmpty()) error = "invoice: Failed to generate invoice email. Could not find special_registration record in the DB, searched for special_registration_id: " + QString::number(special_registration_id);
+
+    QSharedPointer<User> user = UserDao::getUser(db, user_id);
+    if (user.isNull()) error = "Failed to generate invoice email, unable to find relevant data in the database for user_id: " + QString::number(user_id);
 
     if (error == "") {
-        unsigned long long int roles = special_registration["roles"].toULongLong();
-        QString email   = special_registration["email"].toString();
-        int org_id      = special_registration["org_id"].toInt();
-        int admin_id    = special_registration["admin_id"].toInt();
-        QString url     = special_registration["url"].toString();
-
-        QSharedPointer<User> admin = UserDao::getUser(db, admin_id);
-        QMap<QString, QVariant> personal = UserDao::getUserPersonalInfo(db, admin_id);
-
         std::string email_body;
 
-        ctemplate::TemplateDictionary dict("invoice_org");
+        ctemplate::TemplateDictionary dict("invoice");
+        dict.SetValue("USERNAME", Email::htmlspecialchars(user->display_name()));
 
-        QString invoice_link = settings.get("site.url") + url;
-        dict.SetValue("invoice_LINK", invoice_link.toStdString());
-        dict.SetValue("SENDER_NAME", Email::htmlspecialchars((personal["firstName"].toString() + ' ' + personal["lastName"].toString()).toStdString()));
-        dict.SetValue("SENDER_EMAIL", admin->email());
-        dict.SetValue("RECIPIENT_EMAIL", email.toStdString());
+        QString invoice_link = settings.get("site.url") + "https://twbplatform.org/invoice/" + QString::number(badge_id) + "/";
+        dict.SetValue("INVOICE_LINK", invoice_link.toStdString());
 
-        if (org_id == 0) {
-            if (roles&SITE_ADMIN) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_SITE_ADMIN.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
 
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as an admin (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, 0, 0, admin_id, 0, "invoice_to_SITE_ADMIN");
-            } else if (roles&PROJECT_OFFICER) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_PROJECT_OFFICER.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
+        QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice.tpl";
+        ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
 
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as a project officer (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, 0, 0, admin_id, 0, "invoice_to_PROJECT_OFFICER");
-            } else if (roles&COMMUNITY_OFFICER) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_COMMUNITY_OFFICER.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as a community officer (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, 0, 0, admin_id, 0, "invoice_to_COMMUNITY_OFFICER");
-            }
-        } else {
-            QSharedPointer<Organisation> org = OrganisationDao::getOrg(db, org_id);
-            dict.SetValue("ORG_NAME", org->name());
-
-            if (roles&NGO_ADMIN) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_NGO_ADMIN.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as an organization admin (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, org_id, 0, admin_id, 0, "invoice_to_NGO_ADMIN");
-            } else if (roles&NGO_PROJECT_OFFICER) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_NGO_PROJECT_OFFICER.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as an organization project officer (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, org_id, 0, admin_id, 0, "invoice_to_NGO_PROJECT_OFFICER");
-            } else if (roles&NGO_LINGUIST) {
-                QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/invoice_NGO_LINGUIST.tpl";
-                ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-
-                UserDao::queue_email(db, 0, email, "Invitation to join the TWB platform as an organization linguist (expires in 30 days)", QString::fromUtf8(email_body.c_str()));
-                UserDao::log_email_sent(db, 0, 0, 0, org_id, 0, admin_id, 0, "invoice_to_NGO_LINGUIST");
-            }
-        }
+        UserDao::queue_email(db, user_id, QString::fromStdString(user->email()), "Invoice from Clear Global", QString::fromUtf8(email_body.c_str()), LOW);
+        UserDao::log_email_sent(db, user_id, 0, 0, 0, 0, 0, 0, "invoice_to_volunteer");
     } else {
         IEmailGenerator::generateErrorEmail(error);
     }
---
+++ templates/emails/invoice.tpl
@@ -0,0 +1,21 @@
+<p>
+{{USERNAME}},
+</p>
+<p>
+You have a new draft invoice from Clear Global for the work you recently did.
+</p>
+<p>
+The work is outlined in the invoice: <a href="{{INVOICE_LINK}}">{{INVOICE_LINK}}</a>
+</p>
+<p>
+You will need to login to TWB Platform to access this invoice.
+</p>
+<p>
+Please review this invoice and download it and then email it to <a href="mailto:linguistspayments@clearglobal.org">linguistspayments@clearglobal.org</a> so that the payment can be processed.
+</p>
+<p>
+Best Regards
+</p>
+<p>
+The TWB Team
+</p>
