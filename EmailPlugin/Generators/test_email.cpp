ba7ef7262baf8e1746d7ca102bae44aee9912bb0
reinelic <reinelic@yahoo.fr>
2025-04-30 15:36:38 +0200

    updated org and project

--- EmailPlugin/Generators/test_email.cpp
+++ EmailPlugin/Generators/test_email.cpp
@@ -53,100 +53,96 @@ void test_email::run(int task_id, int claimant_id)
         int project_id = 0;
         int tasks_within_cutoff = 0;
         foreach (QSharedPointer<Task> task, userTasks) {
-                ctemplate::TemplateDictionary *taskSect = dict.AddSectionDictionary("TASK_SECT");
-                QString taskView = settings.get("site.url") + "task/" + QString::number(task->id()) + "/view/?utm_source=email&utm_medium=stream&utm_campaign=task";
-                taskSect->SetValue("TASK_VIEW", taskView.toStdString());
-                taskSect->SetValue("TASK_TITLE", Email::htmlspecialchars(task->title()));
+            ctemplate::TemplateDictionary *taskSect = dict.AddSectionDictionary("TASK_SECT");
+            QString taskView = settings.get("site.url") + "task/" + QString::number(task->id()) + "/view/?utm_source=email&utm_medium=stream&utm_campaign=task";
+            taskSect->SetValue("TASK_VIEW", taskView.toStdString());
+            taskSect->SetValue("TASK_TITLE", Email::htmlspecialchars(task->title()));
 
-                std::string task_type = "Invalid Type";
-                for (int i = 0; i < task_type_details.size(); i++) {
-                    QMap<QString, QVariant> task_type_detail = task_type_details[i];
-                    if (task->tasktype() == task_type_detail["type_enum"].toInt()) task_type = task_type_detail["type_text"].toString().toStdString();
+            std::string task_type = "Invalid Type";
+            for (int i = 0; i < task_type_details.size(); i++) {
+                QMap<QString, QVariant> task_type_detail = task_type_details[i];
+                if (task->tasktype() == task_type_detail["type_enum"].toInt()) task_type = task_type_detail["type_text"].toString().toStdString();
+            }
+            taskSect->SetValue("TASK_TYPE", task_type);
+          
+            std::string source_languagename = task->sourcelocale().languagename();
+            std::string source_countryname  = task->sourcelocale().countryname();
+            std::string target_languagename = task->targetlocale().languagename();
+            std::string target_countryname  = task->targetlocale().countryname();
+            for (int i = 0; i < selections.size(); i++) {
+                QMap<QString, QVariant> selection = selections[i];
+                if (task->sourcelocale().languagecode() == selection["language_code"].toString().toStdString() && task->sourcelocale().countrycode() == selection["country_code"].toString().toStdString()) {
+                    source_languagename = selection["selection"].toString().toStdString();
+                    source_countryname  = "ANY";
                 }
-                taskSect->SetValue("TASK_TYPE", task_type);
-              
-             
+                if (task->targetlocale().languagecode() == selection["language_code"].toString().toStdString() && task->targetlocale().countrycode() == selection["country_code"].toString().toStdString()) {
+                    target_languagename = selection["selection"].toString().toStdString();
+                    target_countryname  = "ANY";
+                }
+            }
 
-                std::string source_languagename = task->sourcelocale().languagename();
-                std::string source_countryname  = task->sourcelocale().countryname();
-                std::string target_languagename = task->targetlocale().languagename();
-                std::string target_countryname  = task->targetlocale().countryname();
-                for (int i = 0; i < selections.size(); i++) {
-                    QMap<QString, QVariant> selection = selections[i];
-                    if (task->sourcelocale().languagecode() == selection["language_code"].toString().toStdString() && task->sourcelocale().countrycode() == selection["country_code"].toString().toStdString()) {
-                        source_languagename = selection["selection"].toString().toStdString();
-                        source_countryname  = "ANY";
-                    }
-                    if (task->targetlocale().languagecode() == selection["language_code"].toString().toStdString() && task->targetlocale().countrycode() == selection["country_code"].toString().toStdString()) {
-                        target_languagename = selection["selection"].toString().toStdString();
-                        target_countryname  = "ANY";
-                    }
+            if (source_countryname == "ANY") taskSect->SetValue("SOURCE_LANGUAGE", source_languagename);
+            else                             taskSect->SetValue("SOURCE_LANGUAGE", source_languagename + " (" + source_countryname + ")");
+            if (target_countryname == "ANY") taskSect->SetValue("TARGET_LANGUAGE", target_languagename);
+            else                             taskSect->SetValue("TARGET_LANGUAGE", target_languagename + " (" + target_countryname + ")");
+
+            
+            taskSect->SetValue("WORD_COUNT", QString::number(task->wordcount()).toStdString());
+            QString createdTime = QDateTime::fromString(QString::fromStdString(task->createdtime()),
+                       "yyyy-MM-ddTHH:mm:ss.zzz").toString("d MMMM yyyy - hh:mm");
+            // Parse the original deadline
+            QDateTime deadlineDateTime = QDateTime::fromString(QString::fromStdString(task->deadline()), 
+                "yyyy-MM-ddTHH:mm:ss.zzz");
+
+            taskSect->SetValue("CREATED_TIME", createdTime.toStdString());
+            QString deadline = QDateTime::fromString(QString::fromStdString(task->deadline()),
+                    "yyyy-MM-ddTHH:mm:ss.zzz").toString("d MMMM yyyy");
+            taskSect->SetValue("DEADLINE", deadline.toStdString());
+
+            taskSect->SetValue("PREVIOUS_DEADLINE_TIME", TaskDao::max_translation_deadline(db, task));
+
+            QSharedPointer<Project> project = ProjectDao::getProject(db, task->projectid());
+            if (!project.isNull()) {
+                if (task->projectid() != project_id) { // Display first time only
+                    taskSect->ShowSection("DESCRIPTION_SECT");
+                    taskSect->SetValue("PROJECT_DESCRIPTION", Email::clean_project_description(project->description()));
+                    taskSect->SetValue("PROJECT_IMPACT", Email::uiCleanseHTMLNewlineAndTabs(project->impact()));
                 }
 
-                if (source_countryname == "ANY") taskSect->SetValue("SOURCE_LANGUAGE", source_languagename);
-                else                             taskSect->SetValue("SOURCE_LANGUAGE", source_languagename + " (" + source_countryname + ")");
-                if (target_countryname == "ANY") taskSect->SetValue("TARGET_LANGUAGE", target_languagename);
-                else                             taskSect->SetValue("TARGET_LANGUAGE", target_languagename + " (" + target_countryname + ")");
-
-                
-                taskSect->SetValue("WORD_COUNT", QString::number(task->wordcount()).toStdString());
-                QString createdTime = QDateTime::fromString(QString::fromStdString(task->createdtime()),
-                           "yyyy-MM-ddTHH:mm:ss.zzz").toString("d MMMM yyyy - hh:mm");
-                // Parse the original deadline
-                QDateTime deadlineDateTime = QDateTime::fromString(QString::fromStdString(task->deadline()), 
-"yyyy-MM-ddTHH:mm:ss.zzz");
-
-                taskSect->SetValue("CREATED_TIME", createdTime.toStdString());
-                QString deadline = QDateTime::fromString(QString::fromStdString(task->deadline()),
-                        "yyyy-MM-ddTHH:mm:ss.zzz").toString("d MMMM yyyy");
-                taskSect->SetValue("DEADLINE", deadline.toStdString());
-
-                taskSect->SetValue("PREVIOUS_DEADLINE_TIME", TaskDao::max_translation_deadline(db, task));
-                
-
-                QSharedPointer<Project> project = ProjectDao::getProject(db, task->projectid());
-                if (!project.isNull()) {
-                    if (task->projectid() != project_id) { // Display first time only
-                        taskSect->ShowSection("DESCRIPTION_SECT");
-                        taskSect->SetValue("PROJECT_DESCRIPTION", Email::clean_project_description(project->description()));
-                        taskSect->SetValue("PROJECT_IMPACT", Email::uiCleanseHTMLNewlineAndTabs(project->impact()));
-                    }
-
-                    if (task->projectid() != project_id) { // Display first time only
-                        QList<QSharedPointer<Tag> > taskTags = TagDao::getTaskTags(db, task->id());
-                        if (taskTags.count() > 0) {
-                            taskSect->ShowSection("TAGS_SECT");
-                            foreach (QSharedPointer<Tag> tag, taskTags) {
-                                ctemplate::TemplateDictionary *tagsList = taskSect->AddSectionDictionary("TAGS_LIST");
-                                QString tagDetails = settings.get("site.url") + "tag/" + QString::number(tag->id()) + "/";
-                                tagsList->SetValue("TAG_DETAILS", tagDetails.toStdString());
-                                tagsList->SetValue("TAG_LABEL", Email::htmlspecialchars(tag->label()));
-                            }
+                if (task->projectid() != project_id) { // Display first time only
+                    QList<QSharedPointer<Tag> > taskTags = TagDao::getTaskTags(db, task->id());
+                    if (taskTags.count() > 0) {
+                        taskSect->ShowSection("TAGS_SECT");
+                        foreach (QSharedPointer<Tag> tag, taskTags) {
+                            ctemplate::TemplateDictionary *tagsList = taskSect->AddSectionDictionary("TAGS_LIST");
+                            QString tagDetails = settings.get("site.url") + "tag/" + QString::number(tag->id()) + "/";
+                            tagsList->SetValue("TAG_DETAILS", tagDetails.toStdString());
+                            tagsList->SetValue("TAG_LABEL", Email::htmlspecialchars(tag->label()));
                         }
                     }
-// Display first time only
-                        taskSect->ShowSection("PARTOF_SECT");
-                        QString projectView = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/view/?utm_source=email&utm_medium=stream&utm_campaign=project";
-                        taskSect->SetValue("PROJECT_VIEW", projectView.toStdString());
-                        taskSect->SetValue("PROJECT_TITLE", Email::htmlspecialchars(project->title()));
-                        QSharedPointer<Organisation> org = OrganisationDao::getOrg(db, project->organisationid());
-                        if (!org.isNull()) {
-                            QString orgView = settings.get("site.url") + "org/" + QString::number(project->organisationid()) + "/profile/?utm_source=email&utm_medium=stream&utm_campaign=org";
-                            taskSect->SetValue("ORG_VIEW", orgView.toStdString());
-                            taskSect->SetValue("ORG_NAME", org->name());
-                        }
+                }
+                // Display first time only
+                taskSect->ShowSection("PARTOF_SECT");
+                QString projectView = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/view/?utm_source=email&utm_medium=stream&utm_campaign=project";
+                taskSect->SetValue("PROJECT_VIEW", projectView.toStdString());
+                taskSect->SetValue("PROJECT_TITLE", Email::htmlspecialchars(project->title()));
+                QSharedPointer<Organisation> org = OrganisationDao::getOrg(db, project->organisationid());
+                if (!org.isNull()) {
+                    QString orgView = settings.get("site.url") + "org/" + QString::number(project->organisationid()) + "/profile/?utm_source=email&utm_medium=stream&utm_campaign=org";
+                    taskSect->SetValue("ORG_VIEW", orgView.toStdString());
+                    taskSect->SetValue("ORG_NAME", org->name());
+                }
 
-                        if (project->imageuploaded() && project->imageapproved()) {
-                            taskSect->ShowSection("IMAGE_SECT");
-                            QString projectImage = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/image";
-                            taskSect->SetValue("PROJECT_IMAGE", projectImage.toStdString());
-                        }
-                    
-                
+                if (project->imageuploaded() && project->imageapproved()) {
+                    taskSect->ShowSection("IMAGE_SECT");
+                    QString projectImage = settings.get("site.url") + "project/" + QString::number(task->projectid()) + "/image";
+                    taskSect->SetValue("PROJECT_IMAGE", projectImage.toStdString());
+                }
+            }
 
-                project_id = task->projectid();
-qDebug() << "project_id:" << project_id << "tasks_within_cutoff:" << tasks_within_cutoff; //(**)
-                tasks_within_cutoff++;
+            project_id = task->projectid();
+            qDebug() << "project_id:" << project_id << "tasks_within_cutoff:" << tasks_within_cutoff; //(**)
+            tasks_within_cutoff++;
         }
 
         if (tasks_within_cutoff > 0) {
@@ -163,7 +159,7 @@ qDebug() << "project_id:" << project_id << "tasks_within_cutoff:" << tasks_withi
             std::string email_body;
             QString templateLocation = QString(TEMPLATE_DIRECTORY) + "emails/user-task-stream.tpl";
             ctemplate::ExpandTemplate(templateLocation.toStdString(), ctemplate::DO_NOT_STRIP, &dict, &email_body);
-qDebug() << "body:" << QString::fromUtf8(email_body.c_str());//(**)
+            qDebug() << "body:" << QString::fromUtf8(email_body.c_str());//(**)
 
             UserDao::queue_email(db, claimant_id, QString::fromStdString(user->email()), "TWB dev: Task Stream", QString::fromUtf8(email_body.c_str()), LOW);
         } else {
@@ -174,5 +170,4 @@ qDebug() << "body:" << QString::fromUtf8(email_body.c_str());//(**)
             IEmailGenerator::generateErrorEmail(error);
         }
     }
-}
 }
